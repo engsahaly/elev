6646642b98cc12911aa12a928fcaa22aada91551
engsahaly <engsahaly@gmail.com>
2022-12-27 22:29:44 +0200

    before closing

--- resources/views/dashboard/modals/spinner.blade.php
+++ resources/views/dashboard/modals/spinner.blade.php
@@ -1,23 +1,23 @@
 <style>
     .spinner-grow {
-        width: 0.8rem !important;
-        height: 0.8rem !important;
+        width: 3rem !important;
+        height: 3rem !important;
     }
 </style>
 
 <div class="text-center">
-    <div class="spinner-grow text-primary m-1" role="status">
+    <div class="spinner-grow text-primary" role="status">
     </div>
-    <div class="spinner-grow text-secondary m-1" role="status">
+    <div class="spinner-grow text-secondary" role="status">
     </div>
-    <div class="spinner-grow text-success m-1" role="status">
+    <div class="spinner-grow text-success" role="status">
     </div>
-    <div class="spinner-grow text-info m-1" role="status">
+    <div class="spinner-grow text-info" role="status">
     </div>
-    <div class="spinner-grow text-warning m-1" role="status">
+    <div class="spinner-grow text-warning" role="status">
     </div>
-    <div class="spinner-grow text-danger m-1" role="status">
+    <div class="spinner-grow text-danger" role="status">
     </div>
-    <div class="spinner-grow text-dark m-1" role="status">
+    <div class="spinner-grow text-dark" role="status">
     </div>
 </div>
