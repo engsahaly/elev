b491771161f2defcceb8046f0bd86bb677e11a45
engsahaly <engsahaly@gmail.com>
2022-12-27 07:53:13 +0200

    add lang files

---
+++ lang/ar/lang.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+    /*
+    | PUBLIC NAMES
+    */
+    'company_name' => 'Elev 8',
+    'home'         => 'الرئيسية',
+    'actions'      => 'الاجراءات',
+    'created_at'   => 'انشئت فى',
+    'add_new'      => 'اضافة جديد',
+    'please_enter' => 'ادخل',
+    'submit'       => 'حفظ',
+    'close'        => 'اغلاق',
+    'edit'         => 'تعديل',
+    'show'         => 'عرض',
+    'delete'       => 'حذف',
+    'filteration'  => 'معايير البحث',
+    'filter'       => 'بحث',
+    'word'         => 'كلمة البحث',
+    'active'       => 'مفعل',
+    'inactive'     => 'غير مفعل',
+    'order'        => 'ترتيب بواسطة',
+    'select_order' => 'اختار الترتيب',
+    'sort'         => 'الترتيب',
+    'perpage'      => 'الصفوف',
+    'asc'          => 'تصاعدى',
+    'desc'         => 'تنازلى',
+    'default_date' => 'الافتراضى (التاريخ)',
+    'default'      => 'الافتراضى',
+    'date'         => 'التاريخ',
+    'date_from'    => 'التاريخ (من)',
+    'date_to'      => 'التاريخ (الى)',
+
+    /*
+    | AUTH MODULE
+    */
+    'login_title'        => 'تسجيل دخول المديرين',
+    'login_title_client' => 'تسجيل دخول العملاء',
+    'welcome_back'       => 'مرحبا مجددا !',
+    'login_quote'        => 'قم بتسجيل الدخول الى لوحة تحكم المديرين !',
+    'login_quote_client' => 'قم بتسجيل الدخول الى لوحة تحكم العملاء !',
+    'email'              => 'البريد الالكترونى',
+    'password'           => 'كلمة السر',
+    'remember_me'        => 'تذكرنى',
+    'login_btn'          => 'تسجيل الدخول',
+    'logout'             => 'تسجيل الخروج',
+    'crafted_by'         => 'تمت البرمجة',
+    'by'                 => 'بواسطة',
+
+];
---
+++ lang/en/lang.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+
+    /*
+    | PUBLIC NAMES
+    */
+    'company_name' => 'Elev 8',
+    'home'         => 'Home',
+    'actions'      => 'Actions',
+    'created_at'   => 'Created At',
+    'add_new'      => 'Add New',
+    'please_enter' => 'Please enter',
+    'submit'       => 'Submit',
+    'close'        => 'Close',
+    'edit'         => 'Edit',
+    'show'         => 'Show',
+    'delete'       => 'Delete',
+    'filteration'  => 'Filteration',
+    'filter'       => 'Filter',
+    'word'         => 'Word',
+    'active'       => 'Active',
+    'inactive'     => 'In Active',
+    'order'        => 'Order By',
+    'select_order' => 'Select Order',
+    'sort'         => 'Sort',
+    'perpage'      => 'Per Page',
+    'asc'          => 'Ascending',
+    'desc'         => 'Descending',
+    'default_date' => 'Default (Date)',
+    'default'      => 'Default',
+    'date'         => 'Date',
+    'date_from'    => 'Date From',
+    'date_to'      => 'Date To',
+    
+    /*
+    | DATATABLE BUTTON NAMES
+    */
+    'copy'      => 'Copy',
+
+    /*
+    | AUTH MODULE
+    */
+    'login_title'        => 'Dashboard Login',
+    'login_title_client' => 'Client Login',
+    'welcome_back'       => 'Welcome Back !',
+    'login_quote'        => 'Sign in to admin dashborad',
+    'login_quote_client' => 'Sign in to client dashborad',
+    'welcome_back'       => 'Welcome Back !',
+    'email'              => 'Email',
+    'password'           => 'Password',
+    'remember_me'        => 'Remember me',
+    'login_btn'          => 'Log In',
+    'logout'             => 'Logout',
+    'crafted_by'         => 'Crafted with',
+    'by'                 => 'by',
+];
