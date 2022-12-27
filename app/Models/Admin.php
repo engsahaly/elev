82f831f27941fc0b64c5693882dbc50df3a9f667
engsahaly <engsahaly@gmail.com>
2022-12-27 08:31:01 +0200

    install spatie permissions package

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use App\Enums\AdminStatuses;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
