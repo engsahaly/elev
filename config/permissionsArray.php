562f3c9d937b05b324a9775ba286b61a9c9f77b2
engsahaly <engsahaly@gmail.com>
2022-12-28 06:08:03 +0200

    add permission seeder with permission group

---
+++ config/permissionsArray.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    // ROLES
+    [
+        'group_name' => 'Roles',
+        'group_value' => 'roles',
+        'permissions' => [
+            'list_roles',
+            'add_roles',
+            'edit_roles',
+            'show_roles',
+            'delete_roles',
+        ],
+    ],
+
+    // ADMINS
+    [
+        'group_name' => 'Admins',
+        'group_value' => 'admins',
+        'permissions' => [
+            'list_admins',
+            'add_admins',
+            'edit_admins',
+            'show_admins',
+            'delete_admins',
+        ],
+    ],
+    
+];
